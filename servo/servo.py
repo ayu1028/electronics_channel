a28c1a6b1d4f009e9b0f6623997351198d369164
山盛歩実 <ayumiatmtlab@gmail.com>
2026-07-11 15:06:28 +0900

    program update

--- servo/servo.py
+++ servo/servo.py
@@ -1,24 +1,25 @@
 from machine import Pin, PWM
 from time import sleep_ms
 
-# Power on wait time for sensor
-power_pin = Pin(25, Pin.OUT)
-power_pin.value(0)
-
-# power LED pin
-power_pin.value(1)
-
-# PWM Pin
+# ピン割り当て
 pwm_pin = Pin(28)
 
+# パラメータ設定
+period = 20 #ms
+min_pulse = 0.5 #ms pulse @ −90 deg
+max_pulse = 2.4 #ms pulse @ +90 deg
+
+# サーボモータ定義
 servo = PWM(pwm_pin)
-servo.freq(50)
-duty_base = 65535/20
+servo.freq(1/period*1000)
+duty_base = 65535/period
 
+# 関数定義
 def rotate_servo(deg):
-    duty = 1.45+0.0105*deg
-    servo.duty_u16(int(duty_base * duty))
+    pulse = min_pulse+(max_pulse-min_pulse)*((deg+90)/180)
+    servo.duty_u16(int(duty_base * pulse))
 
+# Main loop
 try:
     while True:
         rotate_servo(0)
@@ -30,5 +31,6 @@ try:
         rotate_servo(-85)
         sleep_ms(1000)
 
+    
 except KeyboardInterrupt:
     print('finish')
